2a80a74fa6d59ea38bec5e8a8f25332324e23869
Marcin Bigos <marcin.bigos99@gmail.com>
2020-03-28 16:18:00 +0100

    initialize progressor library

---
+++ Devscord.Progressor/Render.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devscord.Progressor
+{
+    public static class Render
+    {
+        public static string Bar()
+        {
+            return default;
+        }
+    }
+
+
+}
